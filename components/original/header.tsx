5c1884d058e8eb79b24deed169b37ab2ca2f2d0d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-22 18:39:20 +0900

    feat: margin change

--- components/original/header.tsx
+++ components/original/header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
   }
 
   return (
-    <header className="mt-5">
+    <header className="mt-5 mb-4">
       <div className="container max-w-4xl flex justify-between">
         <div className="flex items-center">
           <div className="mr-4">
