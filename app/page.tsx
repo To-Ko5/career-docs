c11833ce42958fbf8c6461b5757e0469d5ccbc09
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-23 11:15:25 +0900

    feat: about component add

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+import About from '@/components/original/about'
 import Date from '@/components/original/date'
 import Profile from '@/components/original/profile'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="container max-w-4xl">
-      <div className="mb-4">
+    <main className="container max-w-4xl space-y-10">
+      <div>
         <Date />
       </div>
       <div>
         <Profile />
+      </div>
+      <div>
+        <About />
       </div>
     </main>
   )
---
+++ components/original/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+      <h2 className="text-xl font-bold mb-2">About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At totam
+        commodi impedit numquam sunt veritatis debitis asperiores iste alias.
+        Sequi ipsam voluptatem praesentium in? Laudantium magni quo sint
+        asperiores voluptatibus.
+      </p>
+    </section>
+  )
+}
+
+export default About
