e528aa9593dc25c19267444203e95c0fa0fb0d22
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-26 16:17:37 +0900

    feat: footer style add

--- components/original/footer.tsx
+++ components/original/footer.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { meta } from '@/data/data'
+import Link from 'next/link'
 
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className="p-3 border-t">
+      <p className="text-center text-sm text-muted-foreground">
+        <Link href="/">{meta.title}</Link>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
