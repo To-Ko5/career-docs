daa6d3f63a4fa5d09e531bf06a5218040f3086a0
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-27 15:52:50 +0900

    feat: work title add

--- components/original/work.tsx
+++ components/original/work.tsx
@@ -8,20 +8,37 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import Link from 'next/link'
 
 const Work = () => {
+  const work = {
+    start: 2023,
+    end: 2024
+  }
+
   return (
     <section>
       <Card>
         <CardHeader>
-          <CardTitle className="text-xl font-bold">Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <div className="text-sm text-right tabular-nums text-muted-foreground sm:hidden">
+            {work.start} - {work.end}
+          </div>
+          <div className="flex items-center justify-between gap-x-2">
+            <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+              <Link className="hover:underline" href="/">
+                Projects
+              </Link>
+            </h3>
+            <div className="text-sm tabular-nums text-muted-foreground hidden sm:block">
+              {work.start} - {work.end}
+            </div>
+          </div>
         </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
+        <CardContent className="text-sm text-muted-foreground">
+          Card Content
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
+        <CardFooter className="text-sm text-muted-foreground">
+          Card Footer
         </CardFooter>
       </Card>
     </section>
