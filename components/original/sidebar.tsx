f559001b273ad1e15ef8f16205dbae8a39ad6d57
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-01-30 16:45:45 +0900

    feat: sidebar content add

--- components/original/sidebar.tsx
+++ components/original/sidebar.tsx
@@ -7,8 +7,8 @@ import {
   SheetTitle,
   SheetTrigger
 } from '@/components/ui/sheet'
-import { Button } from '../ui/button'
-import { MenuIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { MenuIcon, MoreHorizontalIcon } from 'lucide-react'
 
 const Sidebar = () => {
   return (
@@ -20,8 +20,24 @@ const Sidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side={'left'}>
+        <SheetHeader className="mb-4">
+          <SheetTitle>Case 1</SheetTitle>
+          <SheetDescription className="flex gap-x-2">
+            <p className="line-clamp-2 mb-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A
+              possimus assumenda at consectetur natus, dolore itaque error
+              officiis deserunt ab unde excepturi beatae ipsam cum quod repellat
+              reprehenderit. At, quibusdam.
+            </p>
+            <div className="text-right">
+              <Button variant="outline" size="icon">
+                <MoreHorizontalIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+              </Button>
+            </div>
+          </SheetDescription>
+        </SheetHeader>
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetTitle>Case 2</SheetTitle>
           <SheetDescription>
             This action cannot be undone. This will permanently delete your
             account and remove your data from our servers.
