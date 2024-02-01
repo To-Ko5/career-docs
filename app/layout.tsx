25c28efe16176be0e714172cdb86377e2cba254d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-01 18:42:16 +0900

    feat: layout style add

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
-          <div>{children}</div>
-          <Footer />
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <div className="flex-grow">{children}</div>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
