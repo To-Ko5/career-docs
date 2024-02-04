a96c7082772239137d2aacfe661b3e0fefe3c008
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-04 14:05:31 +0900

    refactor: html refactor

--- components/original/sidebar.tsx
+++ components/original/sidebar.tsx
@@ -17,6 +17,7 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog'
+import Link from 'next/link'
 
 const Sidebar = () => {
   return (
@@ -30,32 +31,21 @@ const Sidebar = () => {
       <SheetContent side={'left'}>
         <SheetHeader className="mb-4">
           <SheetTitle>Case 1</SheetTitle>
-          <SheetDescription className="flex gap-x-2">
-            <p className="line-clamp-2 mb-2">
+          <div className="flex gap-x-2">
+            <SheetDescription className="line-clamp-2 mb-2">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A
               possimus assumenda at consectetur natus, dolore itaque error
               officiis deserunt ab unde excepturi beatae ipsam cum quod repellat
               reprehenderit. At, quibusdam.
-            </p>
+            </SheetDescription>
             <div className="text-right">
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant="outline" size="icon">
-                    <MoreHorizontalIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="max-w-screen-xl w-4/6">
-                  <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                      This action cannot be undone. This will permanently delete
-                      your account and remove your data from our servers.
-                    </DialogDescription>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
+              <Button variant="outline" size="icon" asChild>
+                <Link href={`/case/1`}>
+                  <MoreHorizontalIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                </Link>
+              </Button>
             </div>
-          </SheetDescription>
+          </div>
         </SheetHeader>
       </SheetContent>
     </Sheet>
