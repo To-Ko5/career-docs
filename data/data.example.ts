9f1e5b5c1b0f73fedd53c4f68d49f2d8b46f05c5
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-06 11:31:50 +0900

    feat: example file add

---
+++ data/data.example.ts
@@ -0,0 +1,14 @@
+export const site = {
+  title: 'site description',
+  description: 'site description'
+}
+
+export const date = {
+  lastUpdatedAt: new Date()
+}
+
+export const about = {
+  content: 'lorem ipsum dolor sit amet consectetur adipisicing elitt totam'
+}
+
+export const Works = []
