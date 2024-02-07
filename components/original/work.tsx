217e7f500265d9e1b3e75b7eaab7aafd0e825a36
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-07 20:36:01 +0900

    feat: work data set

--- components/original/work.tsx
+++ components/original/work.tsx
@@ -1,50 +1,73 @@
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { work } from '@/types/data'
 import Link from 'next/link'
 
-const Work = () => {
-  const work = {
-    start: 2023,
-    end: 2024
-  }
+type Props = {
+  work: work
+}
 
+const Work = ({ work }: Props) => {
   return (
     <section>
       <Card>
         <CardHeader>
           <div className="text-sm text-right tabular-nums text-muted-foreground sm:hidden">
-            {work.start} - {work.end}
+            {work.date.start} - {work.date.end}
           </div>
           <div className="flex items-center justify-between gap-x-2">
             <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none mb-2">
-              <Link className="hover:underline" scroll={false} href={`/case/1`}>
-                Projects
+              <Link
+                className="hover:underline text-xl"
+                scroll={false}
+                href={`/case/${work.case}`}
+              >
+                {work.projects}
               </Link>
             </h3>
             <div className="text-sm tabular-nums text-muted-foreground hidden sm:block">
-              {work.start} - {work.end}
+              {work.date.start} - {work.date.end}
             </div>
           </div>
-          <div>
-            <Badge variant="outline">Frontend</Badge>
+          <div className="flex gap-2 flex-wrap">
+            {work.positions.map((position, index) => {
+              return (
+                <Badge variant="outline" key={index}>
+                  {position}
+                </Badge>
+              )
+            })}
           </div>
         </CardHeader>
 
         <CardContent>
           <h4 className="text-sm font-semibold mb-2">使用技術</h4>
-          <Badge variant="outline">Next.js</Badge>
+          <div className="flex gap-2 flex-wrap">
+            {work.skills.map((skill, index) => {
+              return (
+                <Badge variant="outline" key={index}>
+                  {skill}
+                </Badge>
+              )
+            })}
+          </div>
         </CardContent>
 
         <CardContent>
           <h4 className="text-sm font-semibold mb-2">使用ツール</h4>
-          <Badge variant="outline">Azure DevOps</Badge>
+          <div className="flex gap-2 flex-wrap">
+            {work.tools.map((tool, index) => {
+              return (
+                <Badge variant="outline" key={index}>
+                  {tool}
+                </Badge>
+              )
+            })}
+          </div>
         </CardContent>
 
         <CardContent className="text-sm text-muted-foreground">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus repellendus cumque explicabo ea molestiae temporibus
-          praesentium tempora. Dolorem esse rerum alias necessitatibus non
-          earum, ducimus, expedita in molestias numquam dicta.
+          {work.description}
         </CardContent>
       </Card>
     </section>
--- components/original/works.tsx
+++ components/original/works.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Work from './work'
+import { works } from '@/data/data'
 
 const Works = () => {
-  const List = [...Array(5)]
   return (
     <section>
       <h2 className="text-xl font-bold mb-4">Works</h2>
 
       <div className="space-y-8">
-        {List.map((index) => {
-          return <Work key={index} />
+        {works.map((work) => {
+          return <Work key={work.case} work={work} />
         })}
       </div>
     </section>
