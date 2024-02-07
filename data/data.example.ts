ee7e4361b5f64429f7a819f954c788d0f83e0941
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-07 20:35:40 +0900

    feat: work data example add

--- data/data.example.ts
+++ data/data.example.ts
@@ -11,4 +11,41 @@ export const about = {
   content: 'lorem ipsum dolor sit amet consectetur adipisicing elitt totam'
 }
 
-export const Works = []
+export const works = [
+  {
+    case: 1,
+    projects: 'case1',
+    date: {
+      start: 2023,
+      end: 2024
+    },
+    positions: ['Frontend'],
+    skills: ['Next.js', 'MUI'],
+    tools: ['GitHub', 'AWS'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing'
+  },
+  {
+    case: 2,
+    projects: 'case2',
+    date: {
+      start: 2023,
+      end: 2024
+    },
+    positions: ['Frontend'],
+    skills: ['Nuxt.js', 'Vuetify'],
+    tools: ['Adobe', 'Azure DevOps'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing'
+  },
+  {
+    case: 3,
+    projects: 'case3',
+    date: {
+      start: 2023,
+      end: 2024
+    },
+    positions: ['Frontend'],
+    skills: ['Next.js', 'Tailwind'],
+    tools: ['Docker', 'Figma'],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing'
+  }
+]
