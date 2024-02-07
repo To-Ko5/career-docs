e28b1e2bda9ed29da34cc7c6bb3d4626cf5415f8
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-07 20:35:11 +0900

    feat: type add

---
+++ types/data.ts
@@ -0,0 +1,12 @@
+export type work = {
+  case: number
+  projects: string
+  date: {
+    start: string
+    end: string
+  }
+  positions: string[]
+  skills: string[]
+  tools: string[]
+  description: string
+}
