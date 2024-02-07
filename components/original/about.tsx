174d8167df052e2ab00c8c3505c7053ed5f0596b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-07 20:03:47 +0900

    refactor: data set

--- components/original/about.tsx
+++ components/original/about.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
+import { about } from '@/data/data'
 
 const About = () => {
   return (
     <section>
       <h2 className="text-xl font-bold mb-2">About</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At totam
-        commodi impedit numquam sunt veritatis debitis asperiores iste alias.
-        Sequi ipsam voluptatem praesentium in? Laudantium magni quo sint
-        asperiores voluptatibus.
-      </p>
+      <p>{about.content}</p>
     </section>
   )
 }
