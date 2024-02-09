225b31cbea83b6c841544dae06ea7a12d4ea75d6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-09 21:38:07 +0900

    refactor: code organize

--- app/case/[id]/page.tsx
+++ app/case/[id]/page.tsx
@@ -1,17 +1,28 @@
 import Date from '@/components/original/date'
 import Work from '@/components/original/work'
 import { Button } from '@/components/ui/button'
+import { site, works } from '@/data/data'
+import { Metadata } from 'next'
 import Link from 'next/link'
-import React from 'react'
+import { notFound } from 'next/navigation'
 
-const CasePage = () => {
+const CasePage = ({ params }: { params: { id: string } }) => {
+  const paramsId = Number(params.id)
+  const work = works.find((work) => {
+    if (work.case === paramsId) {
+      return work
+    }
+  })
+  if (!work) {
+    return notFound()
+  }
   return (
     <main className="container max-w-4xl pb-16 space-y-10">
       <div>
         <Date />
       </div>
       <div>
-        <Work />
+        <Work work={work} />
       </div>
       <div>
         <Button variant="outline">
