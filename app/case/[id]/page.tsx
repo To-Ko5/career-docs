c3efc15eb626585d11f18defe2aa482f5d301883
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-10 14:46:09 +0900

    feat: generateMetadata add

--- app/case/[id]/page.tsx
+++ app/case/[id]/page.tsx
@@ -1,18 +1,43 @@
 import Date from '@/components/original/date'
 import Work from '@/components/original/work'
 import { Button } from '@/components/ui/button'
-import { site, works } from '@/data/data'
+import { profile, site, works } from '@/data/data'
+import { work } from '@/types/data'
 import { Metadata } from 'next'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
-const CasePage = ({ params }: { params: { id: string } }) => {
-  const paramsId = Number(params.id)
-  const work = works.find((work) => {
+type Props = {
+  params: { [key: string]: string }
+}
+
+const setWork = (works: work[], paramsId: number) => {
+  return works.find((work) => {
     if (work.case === paramsId) {
       return work
     }
   })
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const paramsId = Number(params.id)
+  const work = setWork(works, paramsId)
+  if (work) {
+    return {
+      title: `${work.project} | ${site.title}`,
+      description: `${profile.name} | ${work.project}`
+    }
+  } else {
+    return {
+      title: site.title,
+      description: site.description
+    }
+  }
+}
+
+const CasePage = ({ params }: Props) => {
+  const paramsId = Number(params.id)
+  const work = setWork(works, paramsId)
   if (!work) {
     return notFound()
   }
