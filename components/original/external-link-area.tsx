3bebe8560cfdbe681c45c77d69abf7316ec52a9b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-12 11:17:47 +0900

    feat: tooltip add

--- components/original/external-link-area.tsx
+++ components/original/external-link-area.tsx
@@ -1,4 +1,10 @@
 import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
 import { url_link } from '@/types/data'
 import Link from 'next/link'
 
@@ -10,20 +16,46 @@ const ExternalLinkArea = ({ urlLinks }: Props) => {
   return (
     <div className="flex gap-2 flex-wrap">
       {urlLinks.map((link, index) => {
-        return (
-          <div key={index}>
-            <Button
-              variant="outline"
-              size="icon"
-              className="rounded-full"
-              asChild
-            >
-              <Link href={`${link.href}`}>
-                <link.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-              </Link>
-            </Button>
-          </div>
-        )
+        if (link.tooltip) {
+          return (
+            <div key={index}>
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="rounded-full"
+                      asChild
+                    >
+                      <Link href={`${link.href}`}>
+                        <link.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                      </Link>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom" className="text-xs">
+                    <p>{link.tooltip}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          )
+        } else {
+          return (
+            <div key={index}>
+              <Button
+                variant="outline"
+                size="icon"
+                className="rounded-full"
+                asChild
+              >
+                <Link href={`${link.href}`}>
+                  <link.icon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                </Link>
+              </Button>
+            </div>
+          )
+        }
       })}
     </div>
   )
