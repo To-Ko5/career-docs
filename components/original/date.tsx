0769f74e95062cb3e252acece125229dddb3b40b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-14 09:40:41 +0900

    refactor: date format

--- components/original/date.tsx
+++ components/original/date.tsx
@@ -4,7 +4,7 @@ import { date } from '@/data/data'
 const Date = () => {
   return (
     <p className="text-right text-sm text-muted-foreground mb-4">
-      <time>{format(date.lastUpdatedAt, 'yyyy-MM-dd')}</time>
+      <time>{format(date.lastUpdatedAt, 'yyyy/MM/dd')}</time>
     </p>
   )
 }
