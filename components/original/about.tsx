bed8e435b429fca74ba53d7f488f68855b94549a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-14 09:40:27 +0900

    refactor: css add

--- components/original/about.tsx
+++ components/original/about.tsx
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <section>
       <h2 className="text-xl font-bold mb-2">About</h2>
-      <p>{about.content}</p>
+      <p className="whitespace-pre-line text-muted-foreground">
+        {about.content}
+      </p>
     </section>
   )
 }
