461903541d359c02f11e0ee75c82c2ad06516eef
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-02-18 11:25:02 +0900

    feat: pagination check function add

--- components/original/work-pagination.tsx
+++ components/original/work-pagination.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Pagination,
   PaginationContent,
@@ -5,17 +6,35 @@ import {
   PaginationNext,
   PaginationPrevious
 } from '@/components/ui/pagination'
+import { works } from '@/data/data'
+import { useParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
 
 const WorkPagination = () => {
+  const params = useParams()
+  const paramsId = Number(params.id)
+
+  const checkFirstPage = useCallback(() => {
+    return paramsId === 1
+  }, [paramsId])
+
+  const checkEndPage = useCallback(() => {
+    return paramsId === works.length
+  }, [paramsId])
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
+        {!checkFirstPage() && (
+          <PaginationItem>
+            <PaginationPrevious href={`${paramsId - 1}`} />
+          </PaginationItem>
+        )}
+        {!checkEndPage() && (
+          <PaginationItem>
+            <PaginationNext href={`${paramsId + 1}`} />
+          </PaginationItem>
+        )}
       </PaginationContent>
     </Pagination>
   )
